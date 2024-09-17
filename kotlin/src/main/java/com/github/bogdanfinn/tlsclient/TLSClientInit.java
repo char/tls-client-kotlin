e54eaab6763ccd41ef8c47e2efdda2436a4881cb
Charlotte Som <charlotte@som.codes>
2024-09-17 19:43:03 +0100

    fall back to HTTP cdn if the resource is not bundled with the JAR

--- kotlin/src/main/java/com/github/bogdanfinn/tlsclient/TLSClientInit.java
+++ kotlin/src/main/java/com/github/bogdanfinn/tlsclient/TLSClientInit.java
@@ -4,41 +4,68 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
 
 public class TLSClientInit {
-    public static void init() throws IOException {
+    private static boolean initialized;
+
+    private static String getLibName() {
         String osName = System.getProperty("os.name");
         String osArch = System.getProperty("os.arch");
 
-        String libName = null;
         if (osName.contains("Windows")) {
-            libName = "tls-client-windows-" + osArch + ".dll";
+            return "tls-client-windows-" + osArch + ".dll";
         } else if (osName.contains("Linux")) {
-            libName = "tls-client-linux-" + osArch + ".so";
+            return "tls-client-linux-" + osArch + ".so";
         } else if (osName.contains("Mac")) {
-            libName = "tls-client-darwin-" + osArch + ".dylib";
+            return "tls-client-darwin-" + osArch + ".dylib";
         }
 
-        if (libName == null) throw new IOException("Unsupported platform.");
+        throw new UnsupportedOperationException("Loading tls-client is not supported on this platform.");
+    }
+
+    public static void initFromPath(File base) {
+        if (initialized) return;
+        System.load(new File(base, getLibName()).getAbsolutePath());
+        initialized = true;
+    }
+
+    public static void initFromResource(InputStream stream) throws IOException {
+        if (initialized) return;
+
+        File tempFile = Files.createTempFile("temp_", getLibName()).toFile();
+        tempFile.deleteOnExit();
+
+        try (FileOutputStream out = new FileOutputStream(tempFile)) {
+            stream.transferTo(out);
+        }
+
+        System.load(tempFile.getAbsolutePath());
+
+        initialized = true;
+    }
+
+    public static void init() throws IOException {
+        if (initialized) return;
 
         String tlsClientDev = System.getProperty("com.github.bogdanfinn.tlsclient.dev");
         if (tlsClientDev != null && tlsClientDev.equals("1")) {
-            System.load(new File("../go/dist/" + libName).getAbsolutePath());
+            initFromPath(new File("../go/dist/"));
             return;
         }
 
-        try (InputStream stream = TLSClientInit.class.getResourceAsStream("/com/github/bogdanfinn/tlsclient/resources/" + libName)) {
-            if (stream == null) throw new IOException("Library " + libName + " was not found in resources.");
-
-            File tempFile = Files.createTempFile("temp_", libName).toFile();
-            tempFile.deleteOnExit();
-
-            try (FileOutputStream out = new FileOutputStream(tempFile)) {
-                stream.transferTo(out);
+        try (InputStream stream = TLSClientInit.class.getResourceAsStream("/com/github/bogdanfinn/tlsclient/resources/" + getLibName())) {
+            if (stream != null) {
+                initFromResource(stream);
+                return;
             }
-
-            System.load(tempFile.getAbsolutePath());
         }
+
+        String httpFallbackBase = "https://cdn.kekr.io/tls/binary/"; // provided by kekr
+        HttpURLConnection connection = (HttpURLConnection) new URL(httpFallbackBase + getLibName()).openConnection();
+        connection.setRequestProperty("User-Agent", "Wget/1.13.4 (linux-gnu)"); // i was told to give this to the cdn
+        initFromResource(connection.getInputStream());
     }
 }
